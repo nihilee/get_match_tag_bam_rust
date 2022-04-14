489347f7a79056102364e7ffc8423ad104febb68
hao.xu <hao.xu@simceredx.com>
2022-04-15 00:52:27 +0800

    updae

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use rust_htslib::{bam, bam::Read};
 use std::env;
-use std::str;
+// use std::str;
 use std::fs;
 use std::io::{self, BufRead};
-use std::error;
-
+// use std::error;
 
 extern crate fxhash;
 use fxhash::FxHashSet;
@@ -14,8 +13,17 @@ fn main() {
     // println!("Hello, world!");
 
     let args:Vec<String> = env::args().collect();
-    let a = tag_set(&args[1]).unwrap();
+    // let a = tag_set(&args[1]).unwrap();
     // println!("{:?}", a);
+
+    let f = fs::File::open(&args[1]).unwrap();
+    let reader = io::BufReader::new(f);
+    let mut s = FxHashSet::default();
+
+    for line in reader.lines() {
+        let b: Vec<u8> = line.unwrap().as_bytes().to_vec();
+        s.insert(b);
+    }
 
     let mut bam = bam::Reader::from_path(&args[2]).unwrap();
     let header = bam::Header::from_template(bam.header());
@@ -24,8 +32,8 @@ fn main() {
     for r in bam.records() {
         let record = r.unwrap();
         // println!("{:#?}", record.seq().as_bytes().chunks(38));
-        for c in record.seq().as_bytes().windows(38){
-            if a.contains(str::from_utf8(c).unwrap()){
+        for c in record.seq().encoded.windows(38){
+            if s.contains(c){
                 // println!("{:?}", str::from_utf8(c));
                 // println!("{:?}", str::from_utf8(&record.seq().as_bytes()).unwrap());
                 out.write(&record).unwrap();
@@ -36,16 +44,16 @@ fn main() {
     }
 }
 
-fn tag_set(t: &str) -> Result<FxHashSet<String>, Box<dyn error::Error>> {
-    let a = fs::File::open(t)?;
-    let reader = io::BufReader::new(a);
-    // let mut b = HashSet::new();
-    let mut b = FxHashSet::default();
+// fn tag_set(t: &str) -> Result<FxHashSet<String>, Box<dyn error::Error>> {
+//     let a = fs::File::open(t)?;
+//     let reader = io::BufReader::new(a);
+//     // let mut b = HashSet::new();
+//     let mut b = FxHashSet::default();
 
-    for line in reader.lines() {
-        b.insert(line?);
-    }
+//     for line in reader.lines() {
+//         b.insert(line?);
+//     }
 
-    Ok(b)
-}
+//     Ok(b)
+// }
 
